9fb18cbb5673f3ee621035a41d6e9052e03fc0bd
Duvan Castro <duvancastro1026@gmail.com>
2023-02-11 10:44:39 -0500

    app.js: import file app.html

--- src/todos/app.js
+++ src/todos/app.js
@@ -1,4 +1,4 @@
-
+import html from "./app.html?raw";
 /**
  * 
  * @param {String} elementId 
@@ -9,7 +9,7 @@ export const App = ( elementId ) => {
     (()=> {
 
         const app = document.createElement('div');
-        app.innerHTML = '<h1>Hola Mundo</h1>';
+        app.innerHTML = html;
         document.querySelector( elementId ).append( app );
 
     })();
