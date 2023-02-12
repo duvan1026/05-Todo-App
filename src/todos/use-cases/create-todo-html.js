ce204570f95a944b48ddd3ff0fd62b84100e39fb
Duvan Castro <duvancastro1026@gmail.com>
2023-02-12 09:29:41 -0500

    create-todo-html.js: struturing html in function createTodoHTML with parameters the classList and setAttribute

--- src/todos/use-cases/create-todo-html.js
+++ src/todos/use-cases/create-todo-html.js
@@ -8,10 +8,21 @@ export const createTodoHTML = ( todo ) => {
 
     if( !todo ) throw new Error('A TODO object is requiered');
 
-    const html = `<h1>${ todo.description }</h1>`;
+    const html = ` 
+            <div class="view">
+                <input class="toggle" type="checkbox" ${ todo.done ? 'checked': '' }>
+                <label>Probar JavaScript</label>
+                <button class="destroy"></button>
+            </div>
+            <input class="edit" value="Create a TodoMVC template">
+    `;
 
     const liElement = document.createElement('li');
     liElement.innerHTML = html;
+    liElement.setAttribute( 'data-id', todo.id );
+
+    if ( todo.done )
+        liElement.classList.add('completed');
 
     return liElement;
 }
