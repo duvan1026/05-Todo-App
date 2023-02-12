ecaaf83ec1350abed888a63e79ac6eafad992fcc
Duvan Castro <duvancastro1026@gmail.com>
2023-02-12 09:42:28 -0500

    render-todo.js: validation the elementId and optimitazion element

--- src/todos/use-cases/render-todos.js
+++ src/todos/use-cases/render-todos.js
@@ -1,5 +1,8 @@
 import { createTodoHTML } from "./create-todo-html";
 
+
+let element;
+
 /**
  * 
  * @param {String} elementId 
@@ -7,8 +10,13 @@ import { createTodoHTML } from "./create-todo-html";
  */
 export const renderTodos = ( elementId, todos = [] ) => {
 
-    //TODO: referencia
-    const element = document.querySelector( elementId );
+    if( !element )//Evita estar creando el elemento
+        element = document.querySelector( elementId );
+
+    if( !element ) throw new Error(`Element ${ elementId } not found`); // Validación de elementID
+
+    element.innerHTML = '';// purgo todo su contenido
+
 
     todos.forEach( todo => {
         element.append( createTodoHTML(todo) );
