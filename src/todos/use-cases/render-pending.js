7c3c2ee8476c29d48aed64c42533e6594f984278
Duvan Castro <duvancastro1026@gmail.com>
2023-02-17 04:41:53 -0500

    create file render-pending.js

---
+++ src/todos/use-cases/render-pending.js
@@ -0,0 +1,9 @@
+
+/**
+ * Lugar donde se quiere que se graben los pendientes
+ * @param {String} elementId elemento en el cual voy a construirlo
+ */
+export const renderPending = ( elementId ) => {
+
+    
+}
